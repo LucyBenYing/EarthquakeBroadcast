c1b5197e2df5ca7d9179ed6b9009c7fc046402f2
lby <lbylubenying@163.com>
2019-07-04 16:16:11 +0800

    添加工程

---
+++ earthquakebroadcast/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart'; 
+// import 'package:lib/';
+import 'package:earthquakebroadcast/Home/Views/HomePage.dart'; 
+import 'package:earthquakebroadcast/Home/Push/JPushConfig.dart'; 
+
+
+void main() {
+ setupPush();
+ runApp(
+   EarthquakeApp()  
+ );
+} 
+// void MyApp(){}
+
+void setupPush () {
+   JPushConfig().setupPush();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { 
+    return new Center();
+  }
+  
+}
